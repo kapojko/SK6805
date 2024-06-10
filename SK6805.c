03071087a952b9816e738217e8b766465523b961
Yuriy Kapoyko <y.kapoyko@emitech.tech>
2024-06-10 14:48:12 +0300

    Added SK6805_Transmit function

--- SK6805.c
+++ SK6805.c
@@ -1,5 +1,11 @@
 #include "SK6805.h"
 
+static struct SK6805_Platform *platform;
+
+void SK6805_Init(struct SK6805_Platform *platformPtr) {
+    platform = platformPtr;
+}
+
 uint8_t *SK6805_EncodeLedData(uint32_t grbColor, uint8_t *data) {
     uint32_t mask = 0x800000;
     uint8_t  code4bit = 0;
@@ -23,4 +29,18 @@ uint8_t *SK6805_EncodeLedData(uint32_t grbColor, uint8_t *data) {
 
     // Return a pointer to the next led data
     return &(data[SK6805_LED_ENCODING_SIZE]);
+}
+
+ void SK6805_Transmit(uint32_t grbColors[], int ledsNumber, uint8_t *buf) {
+    // Encode led data
+    uint8_t *data = buf;
+    for (int i = 0; i < ledsNumber; ++i) {
+        data = SK6805_EncodeLedData((uint32_t)grbColors[i], data);
+    }
+
+    // Transmit with SPI
+    platform->spiWrite(buf, SK6805_LED_ENCODING_SIZE * ledsNumber);
+
+    // Delay 80 us, as per datasheet
+    platform->delayUs(80);
 }
--- SK6805.h
+++ SK6805.h
@@ -11,7 +11,19 @@
 // Encoding buffer size for one LED
 #define SK6805_LED_ENCODING_SIZE 12
 
+#define SK6805_LEDS_BUFFER_SIZE(ledsNumber) (ledsNumber * SK6805_LED_ENCODING_SIZE)
+
+struct SK6805_Platform {
+    void (*spiWrite)(uint8_t *data, uint16_t len);
+    void (*delayUs)(uint16_t us);
+};
+
+void SK6805_Init(struct SK6805_Platform *platform);
+
 // Encode single LED data to buffer, return pointer to next LED data
 uint8_t *SK6805_EncodeLedData(uint32_t grbColor, uint8_t *data);
+
+// Encode and transmit leds colors, buf must have size of SK6805_LEDS_BUFFER_SIZE(ledsNumber)
+void SK6805_Transmit(uint32_t grbColors[], int ledsNumber, uint8_t *buf);
 
 #endif // SK6805_H
